87335ad3e1cdb5567b98fb50cef25db1818bbadc
Simi Sebastian <simiesebastian@seqato.com>
2024-07-02 15:12:31 +0530

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'budget-tracker' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('budget-tracker');
-  });
+
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
