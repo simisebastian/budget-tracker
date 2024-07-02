bd9b8fb65167bb4f9001b583090bdf9cabea0b01
Simi Sebastian <simiesebastian@seqato.com>
2024-07-02 16:37:21 +0530

    Adding sign up and login components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    
+];
