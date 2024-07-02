f1c84d3f62a77ff824c54345f92599ea0755f3c7
Simi Sebastian <simiesebastian@seqato.com>
2024-07-02 16:49:37 +0530

    Added AngularMaterialModule.forRoot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AngularMaterialModule } from './angular-material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AngularMaterialModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
