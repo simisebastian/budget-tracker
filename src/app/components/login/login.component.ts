1f6e301004f326f1d2f5a85c363111e3abc6aacb
Simi Sebastian <simiesebastian@seqato.com>
2024-07-03 11:19:01 +0530

    login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { AngularMaterialModule } from '../../angular-material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [AngularMaterialModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  onSubmit(myForm : any): void {}
 }
